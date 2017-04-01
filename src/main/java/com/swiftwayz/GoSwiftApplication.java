4a8da6d412eef331f61ad59a9d8eaa1f564ec721
sydney-mabina <smchauke07@gmail.com>
2017-04-01 21:34:18 +0200

    Creating a maven springboot project shell

---
+++ src/main/java/com/swiftwayz/GoSwiftApplication.java
@@ -0,0 +1,12 @@
+package com.swiftwayz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoSwiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoSwiftApplication.class, args);
+	}
+}
