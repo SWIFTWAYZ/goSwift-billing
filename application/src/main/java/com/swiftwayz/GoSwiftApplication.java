342f73652a7f6c1c2a14f5f5ba4fea9c46ea10bb
sydney-mabina <smchauke07@gmail.com>
2017-04-09 19:27:50 +0200

    enabled componentScan

--- application/src/main/java/com/swiftwayz/GoSwiftApplication.java
+++ application/src/main/java/com/swiftwayz/GoSwiftApplication.java
@@ -2,11 +2,17 @@ package com.swiftwayz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.swiftwayz")
+@EntityScan(basePackages = {"com.swiftwayz.domain"})
+@EnableJpaRepositories(basePackages = {"com.swiftwayz.repository"})
 public class GoSwiftApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GoSwiftApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GoSwiftApplication.class, args);
+    }
 }
