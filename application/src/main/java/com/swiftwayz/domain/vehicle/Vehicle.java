9f3f78dffd2db5fcc67fd1fb4354c1d2b4ff1f1f
sydney-mabina <smchauke07@gmail.com>
2017-04-09 13:44:18 +0200

    adding vehicle domain

---
+++ application/src/main/java/com/swiftwayz/domain/vehicle/Vehicle.java
@@ -0,0 +1,8 @@
+package com.swiftwayz.domain.vehicle;
+
+/**
+ * Created by sydney on 2017/04/08.
+ */
+public class Vehicle {
+
+}
---
+++ application/src/main/java/com/swiftwayz/service/vehicle/VehicleService.java
@@ -0,0 +1,16 @@
+package com.swiftwayz.service.vehicle;
+
+import com.swiftwayz.domain.vehicle.Vehicle;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by sydney on 2017/04/08.
+ */
+@Component
+public class VehicleService {
+
+    public Vehicle add(Vehicle vehicle){
+
+        return  vehicle;
+    }
+}
