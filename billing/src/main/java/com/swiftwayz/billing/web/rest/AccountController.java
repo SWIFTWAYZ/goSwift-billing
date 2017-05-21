2b8e46b07738aa830fc331ad45c8dc7bb0016eb6
sydney-mabina <smchauke07@gmail.com>
2017-05-21 18:01:52 +0200

    Implemented addAccount rest service.

---
+++ billing/src/main/java/com/swiftwayz/billing/web/rest/AccountController.java
@@ -0,0 +1,29 @@
+package com.swiftwayz.billing.web.rest;
+
+import com.swiftwayz.billing.service.AccountService;
+import com.swiftwayz.domain.billing.Account;
+import com.swiftwayz.web.BaseController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by sydney on 2017/05/21.
+ */
+@RestController
+@RequestMapping("/billing")
+public class AccountController extends BaseController{
+
+    @Autowired
+    private AccountService accountService;
+
+    @PostMapping("/account")
+    public ResponseEntity<?> addAccount(@RequestBody Account account){
+        try {
+            Account savedAccount = accountService.addAccount(account);
+            return httpOk(savedAccount);
+        } catch (Exception ex){
+            return httpBadRequest(ex);
+        }
+    }
+}
---
+++ billing/src/test/java/com/swiftwayz/billing/web/rest/AccountControllerIntTest.java
@@ -0,0 +1,93 @@
+package com.swiftwayz.billing.web.rest;
+
+import com.swiftwayz.BillingApplication;
+import com.swiftwayz.billing.service.AccountService;
+import com.swiftwayz.domain.billing.Account;
+import com.swiftwayz.domain.util.Status;
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * Created by sydney on 2017/05/21.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = BillingApplication.class)
+@ActiveProfiles("dev")
+@Transactional
+public class AccountControllerIntTest {
+
+    @Autowired
+    private AccountService accountService;
+
+    private MockMvc restMvc;
+    private MockMvc restMvcMock;
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+        AccountController accountController = new AccountController();
+        ReflectionTestUtils.setField(accountController, "accountService", accountService);
+
+        restMvc = MockMvcBuilders.standaloneSetup(accountController).build();
+    }
+
+    @Test
+    public void should_add_account() throws Exception {
+        Account account = createAccount();
+
+        restMvc.perform(
+                post("/billing/account")
+                        .contentType(TestUtil.APPLICATION_JSON_UTF8)
+                        .content(TestUtil.convertObjectToJsonBytes(account)))
+        .andExpect(status().isOk());
+    }
+
+    @Test
+    public void should_fail_to_add_account_of_invalid_user() throws Exception {
+        Account account = createAccount();
+        account.setUserId(-1L);
+
+        MvcResult mvcResult = restMvc.perform(
+                post("/billing/account")
+                        .contentType(TestUtil.APPLICATION_JSON_UTF8)
+                        .content(TestUtil.convertObjectToJsonBytes(account)))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+
+        String content = mvcResult.getResponse().getContentAsString();
+        Assertions.assertThat(content).contains("User (-1) does not exist.");
+    }
+
+    private Account createAccount() {
+        Account account = new Account();
+
+        account.setBalance(BigDecimal.ZERO);
+        account.setName("goSwift");
+        account.setDescription("goSwift");
+        account.setType("Swift");
+        account.setOpenDate(new Date());
+        account.setStatus(Status.ACTIVE);
+        account.setUserId(Long.valueOf(1));
+        return account;
+    }
+
+}
