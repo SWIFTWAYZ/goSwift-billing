ce5d189fdf443d4f963f4f79a3c321c78ab3d505
sydney-mabina <smchauke07@gmail.com>
2017-06-11 14:27:42 +0200

    Fixed stackoverflow by seting driverDetails list to empty if driver is an owner

--- application/src/main/java/com/swiftwayz/service/driver/adabt/DriverAdaptor.java
+++ application/src/main/java/com/swiftwayz/service/driver/adabt/DriverAdaptor.java
@@ -3,7 +3,11 @@ package com.swiftwayz.service.driver.adabt;
 import com.swiftwayz.domain.user.Driver;
 import com.swiftwayz.domain.user.DriverDetail;
 import com.swiftwayz.domain.user.User;
+import com.swiftwayz.domain.user.VehicleOwner;
 import com.swiftwayz.domain.util.ModelMapper;
+import org.apache.commons.lang3.BooleanUtils;
+
+import java.util.Collections;
 
 /**
  * Created by sydney on 2017/04/23.
@@ -13,6 +17,11 @@ public class DriverAdaptor {
     public static Driver adapt(DriverDetail driverDetail){
         User user = driverDetail.getUser();
         Driver driver = ModelMapper.getMapper().map(user, Driver.class);
+        VehicleOwner vehicleOwner = driverDetail.getVehicleOwner();
+        char isDriver = vehicleOwner.isDriver();
+        if(BooleanUtils.toBoolean(isDriver)){
+            vehicleOwner.setDriverDetails(Collections.emptySet());
+        }
         driver.setDriverDetail(driverDetail);
         return driver;
     }
